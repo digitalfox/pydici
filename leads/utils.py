3bc308db08ffab585770d8fe86c6ed699b6a390a
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2008-10-29 12:21:59 +0100

    Add utils module to factorise some code between views and model

---
+++ leads/utils.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+""" Helper module that factorize some code that would not be
+    appropriate to live in models or view"""
+
+from email.Utils import formatdate
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.Encoders import encode_7or8bit
+
+from django.template.loader import get_template
+from django.template import Context
+
+import pydici.settings
+
+def send_lead_mail(lead):
+    """ Send a mail with lead detailed description.
+    @param lead: the lead to send by mail
+    @type lead: pydici.leads.lead instance
+    @raise exception: if SMTP errors occurs. It is up to the caller to catch that.
+    """
+
+    template=get_template("leads/lead_mail.html")
+
+    msgRoot = MIMEMultipart('related')
+    msgRoot.set_charset("UTF-8")
+    msgRoot["Date"]=formatdate()
+    msgRoot["Subject"]=(u"[AVV] %s : %s" % (lead.client.organisation, lead.name)).encode("UTF-8")
+    msgRoot["From"]=pydici.settings.LEADS_MAIL_FROM
+    msgRoot["To"]=pydici.settings.LEADS_MAIL_TO
+    msgRoot.preamble="This is a multi-part message in MIME format."
+
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+
+    msgText = MIMEText("Le mail au format texte n'est pas encore développé... C'est pour bientôt. La partie HTML est ok")
+    msgText.set_charset("UTF-8")
+    encode_7or8bit(msgText)
+    msgAlternative.attach(msgText)
+
+    msgText = MIMEText(template.render(Context({"lead" : lead, "link_root": pydici.settings.LEADS_MAIL_LINK_ROOT })).encode("UTF-8"), 'html')
+    msgText.set_charset("UTF-8")
+    encode_7or8bit(msgText)
+    msgAlternative.attach(msgText)
+
+    # Let exception be thrown to the caller when smtp host is not reachable
+    smtpConnection = smtplib.SMTP(pydici.settings.LEADS_MAIL_SMTP)
+    smtpConnection.sendmail(pydici.settings.LEADS_MAIL_FROM, pydici.settings.LEADS_MAIL_TO, msgRoot.as_string())
+    smtpConnection.quit()
