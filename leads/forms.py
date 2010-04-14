3c43550a365483673895374cb757aef8f7367d57
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-04-14 19:18:10 +0200

    Reduce default fields size

--- leads/forms.py
+++ leads/forms.py
@@ -22,15 +22,21 @@ class LeadForm(models.ModelForm):
 
 
 class ConsultantStaffingInlineFormset(BaseInlineFormSet):
-    """Custom inline formset used to override mission field (add ajax)"""
+    """Custom inline formset used to override fields"""
     def add_fields(self, form, index):
         """that adds the field in, overwriting the previous default field"""
         super(ConsultantStaffingInlineFormset, self).add_fields(form, index)
-        form.fields["mission"] = AutoCompleteSelectField('mission', required=True)
+        form.fields["mission"] = AutoCompleteSelectField('mission', required=True) # Ajax it
+        form.fields["mission"].widget.attrs.setdefault("size", 8) # Reduce default size
+        form.fields["staffing_date"].widget.attrs.setdefault("size", 10) # Reduce default size
+        form.fields["charge"].widget.attrs.setdefault("size", 3) # Reduce default size
 
 class MissionStaffingInlineFormset(BaseInlineFormSet):
-    """Custom inline formset used to override consultant field (add ajax)"""
+    """Custom inline formset used to override fields"""
     def add_fields(self, form, index):
         """that adds the field in, overwriting the previous default field"""
         super(MissionStaffingInlineFormset, self).add_fields(form, index)
-        form.fields["consultant"] = AutoCompleteSelectField('consultant', required=True)
+        form.fields["consultant"] = AutoCompleteSelectField('consultant', required=True) # Ajax it
+        form.fields["consultant"].widget.attrs.setdefault("size", 8) # Reduce default size
+        form.fields["staffing_date"].widget.attrs.setdefault("size", 10) # Reduce default size
+        form.fields["charge"].widget.attrs.setdefault("size", 3) # Reduce default size
