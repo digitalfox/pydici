1843a3d5dace89a5f797753a48845c3b7a4edf8e
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-08-01 15:20:53 +0200

    Little refactoring: Move some helpers functions to a separate modules

--- staffing/views.py
+++ staffing/views.py
@@ -15,13 +15,13 @@ from django.forms.models import inlineformset_factory
 from django.utils.translation import ugettext as _
 from django.core import urlresolvers
 from django.template import RequestContext
-from django.db import transaction
 
 
 from pydici.staffing.models import Staffing, Mission, Holiday, Timesheet
 from pydici.people.models import Consultant
 from pydici.staffing.forms import ConsultantStaffingInlineFormset, MissionStaffingInlineFormset, TimesheetForm
 from pydici.core.utils import working_days, to_int_or_round
+from pydici.staffing.utils import gatherTimesheetData, saveTimesheetData
 
 def missions(request, onlyActive=True):
     """List of missions"""
@@ -313,6 +313,7 @@ def timesheet(request, consultant_id, year=None, month=None):
         form = TimesheetForm(days=days, missions=missions, forecastTotal=forecastTotal,
                              timesheetTotal=timesheetTotal, initial=timesheetData)
 
+    # Compute workings days of this month and compare it to declared days
     wDays = working_days(month, [h.day for h in Holiday.objects.all()])
     wDaysBalance = wDays - sum(timesheetTotal.values())
 
@@ -354,49 +355,3 @@ def csv_timesheet(request, consultant, days, month, missions):
         writer.writerow(row)
 
     return response
-
-def gatherTimesheetData(consultant, missions, month):
-    """Gather existing timesheet timesheetData
-    @returns: (timesheetData, timesheetTotal)
-    timesheetData represent timesheet form post timesheetData as a dict
-    timesheetTotal is a dict of total charge (key is mission id)"""
-    timesheetData = {}
-    timesheetTotal = {}
-    for mission in missions:
-        for timesheet in Timesheet.objects.select_related().filter(consultant=consultant).filter(mission=mission):
-            if timesheet.working_date.month == month.month:
-                timesheetData["charge_%s_%s" % (timesheet.mission.id, timesheet.working_date.day)] = timesheet.charge
-                if mission.id in timesheetTotal:
-                    timesheetTotal[mission.id] += timesheet.charge
-                else:
-                    timesheetTotal[mission.id] = timesheet.charge
-    return (timesheetData, timesheetTotal)
-
-@transaction.commit_on_success
-def saveTimesheetData(consultant, month, data, oldData):
-    """Save user input timesheet in database"""
-    previousMissionId = 0
-    mission = None
-    for key, charge in data.items():
-        if not charge and not key in oldData:
-            # No charge in new and old data
-            continue
-        if charge and key in oldData and float(data[key]) == oldData[key]:
-            # Data does not changed - skip it
-            continue
-        (foo, missionId, day) = key.split("_")
-        day = int(day)
-        if missionId != previousMissionId:
-            mission = Mission.objects.get(id=missionId)
-            previousMissionId = missionId
-        working_date = month.replace(day=day)
-        timesheet, created = Timesheet.objects.get_or_create(consultant=consultant,
-                                                             mission=mission,
-                                                             working_date=working_date)
-        if charge:
-            # create/update new data
-            timesheet.charge = charge
-            timesheet.save()
-        else:
-            # remove data user just deleted
-            timesheet.delete()
