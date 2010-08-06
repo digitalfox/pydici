c11c7885dd920b0b5dc316d85afd6cdc824f9dc4
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-08-06 14:03:53 +0200

    Filter to keep active and proable missions. Don't use date to find current missions

--- people/views.py
+++ people/views.py
@@ -12,17 +12,20 @@ from django.http import Http404
 from django.template import RequestContext
 
 from pydici.people.models import Consultant
+from pydici.staffing.models import Mission, Staffing
 
 def consultant_detail(request, consultant_id):
     """Summary page of consultant activity"""
-    today = datetime.today()
     try:
         consultant = Consultant.objects.get(id=consultant_id)
         staff = consultant.consultant_set.exclude(id=consultant.id)
-        # Compute user current mission
-        current_staffing = consultant.staffing_set.filter(staffing_date__gte=today)
-        current_missions = list(set(s.mission for s in current_staffing))
-        current_companies = list(set([m.lead.client.organisation.company for m in current_missions if m.lead]))
+        # Compute user current mission based on forecast
+        staffings = Staffing.objects.filter(consultant=consultant)
+        staffings = staffings.filter(mission__nature="PROD")
+        staffings = staffings.filter(mission__probability=100)
+        missions = list(set([s.mission for s in staffings]))
+        companies = list(set([m.lead.client.organisation.company for m in missions if m.lead]))
+        missions = [m for m in missions if m.active] # Only display current missions
 
         leads_as_responsible = set(consultant.lead_responsible.active())
         leads_as_staffee = consultant.lead_set.active()
@@ -31,8 +34,8 @@ def consultant_detail(request, consultant_id):
     return render_to_response("people/consultant_detail.html",
                               {"consultant": consultant,
                                "staff" : staff,
-                               "current_missions" : current_missions,
-                               "current_companies": current_companies,
+                               "missions" : missions,
+                               "companies": companies,
                                "leads_as_responsible" : leads_as_responsible,
                                "leads_as_staffee" : leads_as_staffee,
                                "user": request.user},
