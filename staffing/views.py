7a287a35c4b16d42a8d4ae4661bf3868d19d7b39
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-09-28 18:16:18 +0200

    Add total per consultant

--- staffing/views.py
+++ staffing/views.py
@@ -428,6 +428,12 @@ def all_timesheet(request, year=None, month=None):
             consultantData.append(charges.get((mission.id, consultant.id), 0))
         data.append(consultantData)
     charges = data
+
+    # Compute total per consultant
+    total = [i[1:] for i in charges[1:]]
+    total = zip(*total) # [ [1, 2, 3], [4, 5, 6]... ] => [ [1, 4], [2, 5], [4, 6]...]
+    total = [sum(t) for t in total]
+    charges.append([_("Total")] + total)
     #          , Cons1, Cons2, Cons3
     # Mission 1, M1/C1, M1/C2, M1/C3
     # Mission 2, M2/C1, M2/C2, M2/C3
