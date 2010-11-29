6f76209e54eee937fd4c77ff9760889b1c2b2711
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2010-11-29 12:09:26 +0100

    Add free bill id and a field to mark bills that concern previous year to handle cutoff

--- billing/models.py
+++ billing/models.py
@@ -14,7 +14,6 @@ from datetime import date
 from pydici.leads.models import Lead
 
 class Bill(models.Model):
-    #TODO: add number of bill (unique per lead)
     BILL_STATE = (
             ('1_SENT', _("Sent")),
             ('2_PAID', _("Paid")),
@@ -22,15 +21,20 @@ class Bill(models.Model):
             ('4_CANCELED', _("Canceled")),)
 
     lead = models.ForeignKey(Lead)
+    bill_id = models.CharField(_("Bill id"), max_length=500, blank=True, null=True)
     amount = models.DecimalField(_(u"Amount (k€ excl tax)"), max_digits=10, decimal_places=3)
     due_date = models.DateField(_("Due date"))
     creation_date = models.DateField(_("Creation date"), default=date.today())
     payment_date = models.DateField(_("Payment date"), blank=True, null=True)
     state = models.CharField(_("State"), max_length=30, choices=BILL_STATE, default="1_SENT")
+    previous_year_bill = models.BooleanField(_("Previous year bill"), default=False)
     comment = models.CharField(_("Comments"), max_length=500, blank=True, null=True)
 
     def __unicode__(self):
-        return u"%s/%s" % (self.lead, self.id)
+        if self.bill_id:
+            return u"%s (%s)" % (self.bill_id, self.id)
+        else:
+            return unicode(self.id)
 
     def save(self, force_insert=False, force_update=False):
         if self.state == "2_PAID":
