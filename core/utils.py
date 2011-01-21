a6afdb15c847fc922890b3b2e58ac315b65a14ad
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2011-01-21 09:12:38 +0100

    Use internal django email system to simplify code.

--- core/utils.py
+++ core/utils.py
@@ -7,12 +7,6 @@ appropriate to live in models or view
 @license: GPL v3 or newer
 """
 
-from email.Utils import formatdate
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.Encoders import encode_7or8bit
-from email.Header import Header
 import re
 import os
 from datetime import timedelta
@@ -23,6 +17,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from django.template.loader import get_template
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.core.mail import EmailMultiAlternatives
 
 import pydici.settings
 
@@ -37,39 +32,12 @@ def send_lead_mail(lead, request, fromAddr=pydici.settings.LEADS_MAIL_FROM, from
     @param request: http request of user - used to determine full URL
     @raise exception: if SMTP errors occurs. It is up to the caller to catch that.
     """
-
-    htmlTemplate = get_template("leads/lead_mail.html")
-    textTemplate = get_template("leads/lead_mail.txt")
-
-    msgRoot = MIMEMultipart('related')
-    msgRoot.set_charset("UTF-8")
-    msgRoot["Date"] = formatdate()
-    msgRoot["Subject"] = (u"[AVV] %s : %s" % (lead.client.organisation, lead.name)).encode("UTF-8")
-    msgRoot["From"] = "%s <%s>" % (Header(fromName, "UTF-8"), fromAddr)
-    msgRoot["To"] = pydici.settings.LEADS_MAIL_TO
-    msgRoot.preamble = "This is a multi-part message in MIME format."
-
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-
-    msgText = MIMEText(textTemplate.render(RequestContext(
-                                            request,
-                                            {"obj" : lead })).encode("UTF-8"))
-    msgText.set_charset("UTF-8")
-    encode_7or8bit(msgText)
-    msgAlternative.attach(msgText)
-
-    msgText = MIMEText(htmlTemplate.render(RequestContext(
-                                            request,
-                                            {"obj" : lead })).encode("UTF-8"), 'html')
-    msgText.set_charset("UTF-8")
-    encode_7or8bit(msgText)
-    msgAlternative.attach(msgText)
-
-    # Let exception be thrown to the caller when smtp host is not reachable
-    smtpConnection = smtplib.SMTP(pydici.settings.LEADS_MAIL_SMTP)
-    smtpConnection.sendmail(pydici.settings.LEADS_MAIL_FROM, pydici.settings.LEADS_MAIL_TO, msgRoot.as_string())
-    smtpConnection.quit()
+    subject = u"[AVV] %s : %s" % (lead.client.organisation, lead.name)
+    msgText = get_template("leads/lead_mail.txt").render(RequestContext(request, {"obj" : lead }))
+    msgHtml = get_template("leads/lead_mail.html").render(RequestContext(request, {"obj" : lead }))
+    msg = EmailMultiAlternatives(subject, msgText, fromAddr, [pydici.settings.LEADS_MAIL_TO, ])
+    msg.attach_alternative(msgHtml, "text/html")
+    msg.send()
 
 def capitalize(sentence):
     """
