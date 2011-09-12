115e482d4ae61d1396270063ffd2d10f5619b485
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2011-09-12 21:44:59 +0200

    Ensure lead sales amount is defined when commercial proposal has been sent

--- leads/forms.py
+++ leads/forms.py
@@ -7,6 +7,7 @@ Leads form setup
 
 from django.forms import models
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 
 from ajax_select.fields import AutoCompleteSelectField
 
@@ -21,3 +22,12 @@ class LeadForm(models.ModelForm):
     business_broker = AutoCompleteSelectField('business_broker', required=False, label=_("Business broker"))
     paying_authority = AutoCompleteSelectField('business_broker', required=False, label=_("Paying authority"))
     client = AutoCompleteSelectField('client', required=True, label=_("Client"))
+
+    def clean_sales(self):
+        """Ensure sale amount is defined at lead when commercial proposition has been sent"""
+        if self.cleaned_data["sales"] or self.cleaned_data["state"] in ('QUALIF', 'WRITE_OFFER'):
+            # Sales is defined or we are in early step, nothing to say
+            return self.cleaned_data["sales"]
+        else:
+            # We can't tolerate that sale amount is not known at this step of the process
+            raise ValidationError(_("Sales amount must be defined at this step of the commercial process"))
