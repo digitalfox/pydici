094effb721b95d89f9a230294b41ceb0dc9c3278
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2011-09-16 17:53:52 +0200

    Display leads instead of missions on client page. Make it clearer

--- crm/views.py
+++ crm/views.py
@@ -9,16 +9,17 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from pydici.crm.models import ClientCompany
-from pydici.staffing.models import Mission, Timesheet
+from pydici.staffing.models import Timesheet
+from pydici.leads.models import Lead
 
 def company_detail(request, company_id):
     """Home page of client company"""
 
     company = ClientCompany.objects.get(id=company_id)
 
-    # Find missions of this company
-    missions = Mission.objects.filter(lead__client__organisation__company=company)
-    missions = missions.order_by("lead__client", "lead__state", "lead__start_date")
+    # Find leads of this company
+    leads = Lead.objects.filter(client__organisation__company=company)
+    leads = leads.order_by("client", "state", "start_date")
 
     # Find consultant that work (=declare timesheet) for this company
     consultants = [ s.consultant for s in Timesheet.objects.filter(mission__lead__client__organisation__company=company)]
@@ -27,7 +28,7 @@ def company_detail(request, company_id):
 
     return render_to_response("crm/clientcompany_detail.html",
                               {"company": company,
-                               "missions": missions,
+                               "leads": leads,
                                "consultants": consultants},
                                RequestContext(request))
 
