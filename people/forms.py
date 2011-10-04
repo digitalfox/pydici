2e8eb0f389ac563a7b7204de9d140321b76c5d90
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2011-10-04 22:00:23 +0200

    Check that consultant is subcontractor if subcontractor company is defined

---
+++ people/forms.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+"""
+People form setup
+@author: Sébastien Renard <Sebastien.Renard@digitalfox.org>
+@license: AGPL v3 or newer (http://www.gnu.org/licenses/agpl-3.0.html)
+"""
+
+from django.forms import models
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+
+from ajax_select.fields import AutoCompleteSelectField
+
+from pydici.people.models import Consultant
+
+class ConsultantForm(models.ModelForm):
+    class Meta:
+        model = Consultant
+
+    def clean_subcontractor_company(self):
+        """Ensure subcontractor flag is on if subcontractor company is defined"""
+        if self.cleaned_data["subcontractor_company"] and not self.cleaned_data["subcontractor"]:
+            raise ValidationError(_("Subcontractor company can only be defined for subcontractors"))
+        else:
+            return self.cleaned_data["subcontractor_company"]
