1be54f918ee47d5c8c1e70a8771dc619ed214e2c
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2013-09-15 12:40:43 +0200

    Disable state sorting. Make header translatable

--- expense/tables.py
+++ expense/tables.py
@@ -17,6 +17,7 @@ from core.templatetags.pydici_filters import link_to_consultant
 class ExpenseTable(tables.Table):
     user = tables.Column(verbose_name=_("Consultant"))
     receipt = tables.TemplateColumn("""<a href="{% url 'expense.views.expense_receipt' record.id %}"><img src='{{ MEDIA_URL }}pydici/receipt.png'/></a>""")
+    state = tables.Column(sortable=False, verbose_name=_("State"))
 
     def render_user(self, value):
         return link_to_consultant(value)
