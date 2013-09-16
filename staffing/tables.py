9e538b17f4d477f8519f71607a007fc45fa65c1e
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2013-09-16 23:09:33 +0200

    Inverse bool flag to warn about no staffing / not up to date staffing in order to have red tag when it's wrong and green tick when it's ok

--- staffing/tables.py
+++ staffing/tables.py
@@ -13,15 +13,21 @@ from django_tables2.utils import A
 from staffing.models import Mission
 
 
+class InversedBooleanColumn(tables.BooleanColumn):
+    """Boolean column that inverse True and False"""
+    def render(self, value):
+        return super(InversedBooleanColumn, self).render(not value)
+
+
 class MissionTable(tables.Table):
-    old_forecast = tables.BooleanColumn(accessor="no_staffing_update_since", verbose_name=_("Old fcast"), attrs={"th": {"title": _("Old forecast")}})
-    no_forecast = tables.BooleanColumn(accessor="no_more_staffing_since", verbose_name=_("No fcast"), attrs={"th": {"title": _("No forecast in future")}})
+    old_forecast = InversedBooleanColumn(accessor="no_staffing_update_since", verbose_name=_("Fcast updt"), attrs={"th": {"title": _("Up to date forecast")}})
+    no_forecast = InversedBooleanColumn(accessor="no_more_staffing_since", verbose_name=_("Fcast"), attrs={"th": {"title": _("Forecast in future")}})
     mission_id = tables.Column(accessor="mission_id", verbose_name=_("Mission id"))
     name = tables.LinkColumn(accessor="__unicode__", verbose_name=_("Name"), viewname="staffing.views.mission_home", args=[A("pk")])
     archive = tables.TemplateColumn(template_name="staffing/_mission_table_archive_column.html", verbose_name=_("Archiving"))
 
     class Meta:
         model = Mission
-        sequence = ("name", "nature", "mission_id", "probability", "price", "active", "old_forecast", "no_forecast", "archive")
+        sequence = ("name", "nature", "mission_id", "probability", "price", "active", "no_forecast", "old_forecast", "archive")
         fields = sequence
         attrs = {"class": "pydici-tables2"}
