96a2c466148a6807b1d69d509b19bd84f150f70f
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2013-10-27 18:44:28 +0100

    Add link to expense payment detail

--- expense/tables.py
+++ expense/tables.py
@@ -21,7 +21,9 @@ class ExpenseTable(tables.Table):
     user = tables.Column(verbose_name=_("Consultant"))
     lead = tables.TemplateColumn("""{% if record.lead %}<a href='{% url "leads.views.detail" record.lead.id %}'>{{ record.lead }}</a>{% endif%}""")
     receipt = tables.TemplateColumn("""{% if record.receipt %}<a href="{% url 'expense.views.expense_receipt' record.id %}"><img src='{{ MEDIA_URL }}pydici/receipt.png'/></a>{% endif %}""")
-    state = tables.Column(verbose_name=_("State"))
+    state = tables.TemplateColumn("""{% load i18n %}{% if record.expensePayment %}
+                                                        <a href="{% url 'expense.views.expense_payment_detail' record.id %}">{% trans "Paid" %}</a>
+                                                    {% else %}{{ record.state }}{% endif %}""", verbose_name=_("State"))
 
     def render_user(self, value):
         return link_to_consultant(value)
