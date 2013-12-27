518908774048a443ef4d585ce7ded50668afc0b9
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2013-12-27 23:16:59 +0100

    Don't allow sort on column we cannot do instead of crashing

--- staffing/tables.py
+++ staffing/tables.py
@@ -20,11 +20,11 @@ class InversedBooleanColumn(tables.BooleanColumn):
 
 
 class MissionTable(tables.Table):
-    old_forecast = InversedBooleanColumn(accessor="no_staffing_update_since", verbose_name=_("Fcast updt"), attrs={"th": {"title": _("Up to date forecast")}})
-    no_forecast = InversedBooleanColumn(accessor="no_more_staffing_since", verbose_name=_("Fcast"), attrs={"th": {"title": _("Forecast in future")}})
-    mission_id = tables.Column(accessor="mission_id", verbose_name=_("Mission id"))
-    name = tables.LinkColumn(accessor="__unicode__", verbose_name=_("Name"), viewname="staffing.views.mission_home", args=[A("pk")])
-    archive = tables.TemplateColumn(template_name="staffing/_mission_table_archive_column.html", verbose_name=_("Archiving"))
+    old_forecast = InversedBooleanColumn(accessor="no_staffing_update_since", verbose_name=_("Fcast updt"), attrs={"th": {"title": _("Up to date forecast")}}, orderable=False)
+    no_forecast = InversedBooleanColumn(accessor="no_more_staffing_since", verbose_name=_("Fcast"), attrs={"th": {"title": _("Forecast in future")}}, orderable=False)
+    mission_id = tables.Column(accessor="mission_id", verbose_name=_("Mission id"), orderable=False)
+    name = tables.LinkColumn(accessor="__unicode__", verbose_name=_("Name"), viewname="staffing.views.mission_home", args=[A("pk")], orderable=False)
+    archive = tables.TemplateColumn(template_name="staffing/_mission_table_archive_column.html", verbose_name=_("Archiving"), orderable=False)
 
     class Meta:
         model = Mission
