deefc1a2750944a55b59a00ede5a2ca11655594c
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2014-01-22 23:37:53 +0100

    Better type detection for sorting, factorise number detection code and some cleanup

--- media/js/datatables_custom_sort.js
+++ media/js/datatables_custom_sort.js
@@ -7,28 +7,27 @@
 // Add custom sort
 jQuery.extend( jQuery.fn.dataTableExt.oSort, {
 	"numeric-comma-pre": function ( a ) {
-		var x = (a == "-" || a == "—") ? 0 : a.replace( /,/, "." ).replace(/\s+/, "").replace(/\xA0/, "").replace("&nbsp;", "");
-		return parseFloat( x );
+		return parseFloat( filterNumber(a) );
 	},
 
 	"numeric-comma-asc": function ( a, b ) {
-		return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+	    return cmp(a, b);
 	},
 
 	"numeric-comma-desc": function ( a, b ) {
-		return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+	    return cmp(b, a);
 	},
 	
 	"numeric-html-pre": function ( a ) {
-        return parseFloat($(a).text());
+        return parseFloat($(filterNumber(a)).text());
 	},
 	
 	"numeric-html-asc": function ( a, b ) {
-        return ((a < b) ? -1 : ((a > b) ?  1 : 0));
+        return cmp(a, b);
     },
 
     "numeric-html-desc": function ( a, b ) {
-        return ((a < b) ? 1 : ((a > b) ?  -1 : 0));
+        return cmp(b, a);
     },
 
     "title-numeric-pre": function ( a ) {
@@ -37,46 +36,38 @@ jQuery.extend( jQuery.fn.dataTableExt.oSort, {
     },
  
     "title-numeric-asc": function ( a, b ) {
-        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        return cmp(a, b);
     },
  
     "title-numeric-desc": function ( a, b ) {
-        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        return cmp(b, a);
     }
 } );
 
 // Add type detection
 jQuery.fn.dataTableExt.aTypes.unshift(
         function ( sData ) {
-                var sValidChars = "0123456789,.— \xA0";
-                var Char;
-                var bDecimal = false;
-                var iStart=0;
-                sData = sData.replace("&nbsp;", "");
-
-                /* Negative sign is valid - shift the number check start point */
-                if ( sData.charAt(0) === '-' ) {
-                        iStart = 1;
+                if (isNumber(sData)) {
+                    return 'numeric-comma';
+                } else {
+                    return null;
                 }
-
-                /* Check the numeric part */
-                for ( i=iStart ; i<sData.length ; i++ ) {
-                        Char = sData.charAt(i);
-                        if (sValidChars.indexOf(Char) == -1) {
-                            return null;
-                        }
-                }
-                return 'numeric-comma';
         }
 );
 
 jQuery.fn.dataTableExt.aTypes.unshift(
         function ( sData ) {
+               if (sData.length > 0) {
+                   if (!isNaN(sData[0])) {
+                       // This is a number, not an html fragment, exiting
+                       return null;
+                   }
+               }
                var a = $("<div/>").html(sData).text();  // we need both html() and text() to understand escaped html and extract text from html tags
                if (a == "") {
                    return null;
                }
-               if ((a % 1) == 0) {
+               if (isNumber(a)) {
                    return 'numeric-html';
                } else {
                    return null;
@@ -98,3 +89,35 @@ jQuery.fn.dataTableExt.aTypes.unshift(
                }
         }
 );
+
+// True is x is a number, else False
+function isNumber(x) {
+    var sValidChars = "0123456789,.— \xA0";
+    var Char;
+    var iStart=0;
+    x = x.replace("&nbsp;", "");
+
+    /* Negative sign is valid - shift the number check start point */
+    if ( x.charAt(0) === '-' ) {
+            iStart = 1;
+    }
+
+    /* Check the numeric part */
+    for ( i=iStart ; i < x.length ; i++ ) {
+            Char = x.charAt(i);
+            if (sValidChars.indexOf(Char) == -1) {
+                return false;
+            }
+    }
+    return true;
+}
+
+// Filter a string to prepare its cast to a Int or Float
+function filterNumber(x) {
+    return (x == "-" || x == "—") ? 0 : x.replace( /,/, "." ).replace(/\s+/, "").replace(/\xA0/, "").replace("&nbsp;", "");
+}
+
+// Even most basic things needs to be coded in javascript... sic
+function cmp(a, b) {
+    return ((a < b) ? -1 : ((a > b) ?  1 : 0));
+}
