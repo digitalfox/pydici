4651ab6cb2963af4ed665d89f04cb0ad2563d819
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2014-08-08 23:26:03 +0200

    Add lead postsave hook

---
+++ leads/utils.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+"""
+Helper module that factorize some code that would not be
+appropriate to live in Lead models or view
+@author: Sébastien Renard (sebastien.renard@digitalfox.org)
+@license: AGPL v3 or newer (http://www.gnu.org/licenses/agpl-3.0.html)
+"""
+from django.utils.translation import ugettext
+from django.contrib import messages
+
+from staffing.models import Mission
+from core.utils import send_lead_mail
+
+
+def postSaveLead(request, lead, form, change):
+    mail = False
+    if lead.send_email:
+        mail = True
+        lead.send_email = False
+    lead.save()
+
+    if mail:
+        try:
+            fromAddr = request.user.email or "noreply@noreply.com"
+            send_lead_mail(lead, request, fromAddr=fromAddr,
+                           fromName="%s %s" % (request.user.first_name, request.user.last_name))
+            messages.add_message(request, messages.INFO, ugettext("Lead sent to business mailing list"))
+        except Exception, e:
+            messages.add_message(request, messages.ERROR, ugettext("Failed to send mail: %s") % e)
+
+    # Create or update mission leadect if needed
+    if lead.mission_set.count() == 0:
+        if lead.state in ("OFFER_SENT", "NEGOTIATION", "WON"):
+            mission = Mission(lead=lead)
+            mission.price = lead.sales  # Initialise with lead price
+            mission.subsidiary = lead.subsidiary
+            mission.save()
+            # Create default staffing
+            mission.create_default_staffing()
+            messages.add_message(request, messages.INFO, ugettext("A mission has been initialized for this lead."))
+
+    for mission in lead.mission_set.all():
+        if mission.subsidiary != lead.subsidiary:
+            mission.subsidiary = lead.subsidiary
+            mission.save()
+        if lead.state == "WON":
+            mission.probability = 100
+            mission.active = True
+            mission.save()
+            messages.add_message(request, messages.INFO, ugettext("Mission's probability has been set to 100%"))
+        elif lead.state in ("LOST", "FORGIVEN", "SLEEPING"):
+            mission.probability = 0
+            mission.active = False
+            mission.save()
+            messages.add_message(request, messages.INFO, ugettext("According mission has been archived"))
