3339c30abbd58c73aa725c873be4ed444b035856
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2014-10-11 13:21:25 +0200

    More useful docstring

--- core/forms.py
+++ core/forms.py
@@ -23,19 +23,7 @@ class PydiciSelect2Field():
     empty_values = EMPTY_VALUES
 
     def security_check(self, request, *args, **kwargs):
-        """
-        Returns ``False`` if security check fails.
-
-        :param request: The Ajax request object.
-        :type request: :py:class:`django.http.HttpRequest`
-
-        :param args: The ``*args`` passed to :py:meth:`django.views.generic.base.View.dispatch`.
-        :param kwargs: The ``**kwargs`` passed to :py:meth:`django.views.generic.base.View.dispatch`.
-
-        :return: A boolean value, signalling if check passed or failed.
-        :rtype: :py:obj:`bool`
-
-        """
+        """Restrict access to authenticated and active users only"""
         return request.user.is_authenticated() and request.user.is_active
 
 
