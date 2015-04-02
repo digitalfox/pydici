c4fe2138725b53587daa57308eb2aa431a446183
Aurélien Gâteau <mail@agateau.com>
2015-04-02 10:23:10 +0200

    Add a way for optional apps to extend the menubar

---
+++ core/context_processors.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from importlib import import_module
+
+from django.conf import settings
+
+
+_menu_templates = None
+
+
+def _init_menu_templates():
+    global _menu_templates
+    _menu_templates = defaultdict(list)
+    for app in settings.INSTALLED_APPS:
+        try:
+            mod = import_module(app + '.menus')
+        except ImportError:
+            continue
+        for menu_name, template_name in mod.get_menus().items():
+            _menu_templates[menu_name].append(template_name)
+
+
+class MenuWrapper(object):
+    def __getitem__(self, name):
+        return _menu_templates.get(name)
+
+    def __contains__(self, name):
+        return bool(self[name])
+
+
+def menu(request):
+    if _menu_templates is None:
+        _init_menu_templates()
+    return {
+        'pydici_menu': MenuWrapper(),
+    }
--- pydici/settings.py
+++ pydici/settings.py
@@ -73,7 +73,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.request")
+    "django.core.context_processors.request",
+    "core.context_processors.menu",
+)
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
