e9b87a59bb509c0257a1071c7726bf51d55d3d6d
Sebastien Renard <Sebastien.Renard@digitalfox.org>
2016-04-17 17:40:04 +0200

    Cache session in memcached but also use persistant session storage in db

--- pydici/settings.py
+++ pydici/settings.py
@@ -27,6 +27,8 @@ CACHES = {
     }
 }
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
