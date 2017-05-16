3f9fa9b0e634c292070c8e20e7b498163704d57c
Sebastien Renard <sebastien.renard@digitalfox.org>
2017-05-16 22:35:57 +0200

    exclude node_modules directory from i18n

--- i18n.sh
+++ i18n.sh
@@ -4,6 +4,6 @@
 
 OLD=$PWD
 cd $(dirname $0)
-python manage.py makemessages -a -e ".html,.txt,.py" -i data -i venv
+python manage.py makemessages -a -e ".html,.txt,.py" -i data -i venv -i node_modules
 python manage.py compilemessages
 cd $OLD
