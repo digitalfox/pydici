227a3cf013c40610db4ccf245a6a6f87b336b809
Sebastien Renard <sebastien.renard@digitalfox.org>
2017-08-22 09:09:05 +0200

    remove space in parameter key

---
+++ core/migrations/0004_fix_key.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+def add_default_parameters(apps, schema_editor):
+    Parameter = apps.get_model("core", "Parameter")
+    p = Parameter.objects.get(key="MAIL_FROM ")
+    p.key = "MAIL_FROM"
+    p.save()
+
+
+def remove_default_parameters(apps, schema_editor):
+    Parameter = apps.get_model("core", "Parameter")
+    Parameter = apps.get_model("core", "Parameter")
+    p = Parameter.objects.get(key="MAIL_FROM")
+    p.key = "MAIL_FROM "
+    p.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_fiscal_year'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_default_parameters, remove_default_parameters),
+    ]
