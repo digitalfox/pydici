13527679e8a0db0e09bf07857c949ac2e3156b93
Sebastien Renard <sebastien.renard@digitalfox.org>
2017-08-25 13:09:53 +0200

    Fix description data to fit markdown requirement for bullet list

---
+++ leads/migrations/0002_fix_description_for_markdown.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.db.models import F, Func, Value
+
+def fix_list(apps, schema_editor):
+    Lead = apps.get_model("leads", "Lead")
+    # Use bulk update to avoid triggering default Lead.save, signals and date field auto update.
+    Lead.objects.all().update(description=Func(F("description"), Value(": \n"), Value(":\n"), function="replace"))
+    Lead.objects.all().update(description=Func(F("description"), Value(":\n-"), Value(":\n\n-"), function="replace"))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('leads', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_list),
+    ]
