78a104903400fb76680e5cce1e3c552c73dd6176
Sebastien Renard <sebastien.renard@digitalfox.org>
2018-08-08 11:43:24 +0200

    Add responsibles on in-creation bills table

--- billing/tables.py
+++ billing/tables.py
@@ -5,6 +5,8 @@ Pydici billing tables
 @license: AGPL v3 or newer (http://www.gnu.org/licenses/agpl-3.0.html)
 """
 
+from itertools import chain
+
 from django.db.models import Q
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
@@ -12,11 +14,34 @@ from core.decorator import PydiciFeatureMixin, PydiciNonPublicdMixin
 from core.utils import to_int_or_round
 from crm.views import ThirdPartyMixin
 from billing.models import ClientBill
+from people.models import Consultant
 
 
-class ClientBillTableDT(ThirdPartyMixin, BaseDatatableView):
+class BillTableDT(ThirdPartyMixin, BaseDatatableView):
+    """Base bill table backend for datatables"""
+
+    def filter_queryset(self, qs):
+        """ simple search on some attributes"""
+        search = self.request.GET.get(u'search[value]', None)
+        if search:
+            qs = qs.filter(Q(bill_id__icontains=search) |
+                           Q(lead__deal_id__icontains=search) |
+                           Q(lead__name__icontains=search) |
+                           Q(lead__responsible__name__icontains=search) |
+                           Q(lead__client__organisation__company__name__icontains=search)
+                           )
+        return qs
+
+    def render_column(self, row, column):
+        if column in ("amount", "amount_with_vat"):
+            return to_int_or_round(getattr(row, column), 2)
+        else:
+            return super(BillTableDT, self).render_column(row, column)
+
+
+class ClientBillTableDT(BillTableDT):
     """Client Bill tables backend for datatables"""
-    columns = ("bill_id", "lead", "creation_date", "state", "amount", "amount_with_vat", "comment")
+    columns = ("bill_id", "lead", "responsible", "creation_date", "state", "amount", "amount_with_vat", "comment")
     order_columns = columns
     max_display_length = 20
 
@@ -29,15 +54,22 @@ class ClientBillTableDT(ThirdPartyMixin, BaseDatatableView):
         search = self.request.GET.get(u'search[value]', None)
         if search:
             qs = qs.filter(Q(bill_id__icontains=search) |
+                           Q(state__icontains=search) |
                            Q(lead__deal_id__icontains=search) |
-                           Q(lead__description__icontains=search) |
                            Q(lead__name__icontains=search) |
-                           Q(lead__client__organisation__company__name__icontains=search)
-                           )
+                           Q(lead__responsible__name__icontains=search) |
+                           Q(lead__client__organisation__company__name__icontains=search) |
+                           Q(billdetail__mission__responsible__name__icontains=search)
+                           ).distinct()
         return qs
 
+
     def render_column(self, row, column):
-        if column in ("amount", "amount_with_vat"):
-            return to_int_or_round(getattr(row, column), 2)
+        if column == "responsible":
+            # Get missions and lead responsibles
+            responsibles = ClientBill.objects.filter(id=row.id).values_list("billdetail__mission__responsible__id", "lead__responsible__id")
+            responsibles = set(chain(*responsibles))  # flatten it
+            responsibles = Consultant.objects.filter(id__in=responsibles)
+            return ", ".join([unicode(c) for c in responsibles])
         else:
             return super(ClientBillTableDT, self).render_column(row, column)
