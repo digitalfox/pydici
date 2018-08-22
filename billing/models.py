5b0416034c06d2bd494a51c55adf72c794c70ab0
Sebastien Renard <sebastien.renard@digitalfox.org>
2018-08-22 11:42:02 +0200

    allow to anonymize consultant names on client bills

--- billing/models.py
+++ billing/models.py
@@ -132,6 +132,7 @@ class ClientBill(AbstractBill):
     state = models.CharField(_("State"), max_length=30, choices=CLIENT_BILL_STATE, default="0_DRAFT")
     bill_file = models.FileField(_("File"), max_length=500, upload_to=bill_file_path,
                                  storage=BillStorage(nature="client"), null=True, blank=True)
+    anonymize_profile = models.BooleanField(_("Anonymize profile name"), default=False)
 
     def client(self):
         if self.lead.paying_authority:
