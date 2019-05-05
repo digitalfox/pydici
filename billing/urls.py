36a772a75e51d773765d1afd23a665440e432aa1
Sebastien Renard <sebastien.renard@digitalfox.org>
2019-05-05 23:58:18 +0200

    draft version of client billing control dashboard

--- billing/urls.py
+++ billing/urls.py
@@ -11,6 +11,7 @@ import billing.tables as t
 
 
 billing_urls = [url(r'^bill_review', v.bill_review, name="bill_review"),
+                url(r'^client_billing_control$', v.client_billing_control_pivotable, name='client_billing_control_pivotable'),
                 url(r'^bill_delay', v.bill_payment_delay, name="bill_payment_delay"),
                 url(r'^bill/(?P<bill_id>\d+)/mark_bill_paid$', v.mark_bill_paid, name="mark_bill_paid"),
                 url(r'^file/(?P<nature>.+)/(?P<bill_id>\d+)$', v.bill_file, name="bill_file"),
