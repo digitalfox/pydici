3ff4fb579efa9af5942f04a2a2a7b4c98d569ec5
Sebastien Renard <sebastien.renard@digitalfox.org>
2019-11-15 18:14:10 +0100

    fix misleading description

--- core/models.py
+++ core/models.py
@@ -36,7 +36,7 @@ _FEATURES_CHOICES = (
     ("timesheet_subcontractor", _("Timesheet management of subcontractor")),
     ("billing_management", _("Manage bills, allow to mark bills sent, paid etc.")),
     ("billing_request", _("Create bills and proposed them to billing team ")),
-    ("expense", _("Expense request and management")),
+    ("expense", _("Expense request")),
 )
 
 FEATURES = set([x[0] for x in _FEATURES_CHOICES])
