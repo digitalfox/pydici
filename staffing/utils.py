36f3f87ae5f0c179a612347254ee87255f05b380
Sebastien Renard <sebastien.renard@digitalfox.org>
2020-03-13 10:44:54 +0100

    update yearly mission generation script to handle start/end date boundaries

--- staffing/utils.py
+++ staffing/utils.py
@@ -306,21 +306,23 @@ def timesheet_report_data(mission, start=None, end=None, padding=False):
     return data
 
 
-def create_next_year_std_missions(current, target, dryrun=True):
+def create_next_year_std_missions(current, target, dryrun=True, start_date=None, end_date=None):
     """Create default set of mission for next year based on current holidays and nonprod missions
     @current: current suffix
     @target: target suffix
     @dryrun: save new mission or just print its"""
     #TODO: handle mission date boundaries
     for m in Mission.objects.exclude(nature="PROD").filter(active=True):
-        if not current in m.description:
+        if not m.description or not current in m.description:
             continue
         new_mission = Mission(description = m.description.replace(current, target),
                               subsidiary = m.subsidiary,
                               nature = m.nature,
                               billing_mode = "TIME_SPENT",
                               probability = 100,
-                              probability_auto = False)
+                              probability_auto = False,
+                              start_date = start_date,
+                              end_date = end_date)
         print("Creating new mission %s" % new_mission)
         if not dryrun:
             new_mission.save()
