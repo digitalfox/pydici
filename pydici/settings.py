535a246232f1d8de71b325190566be17e588d3c2
Sebastien Renard <sebastien.renard@digitalfox.org>
2022-02-06 20:08:37 +0100

    remove custom template config. Default is now equivalent

--- pydici/settings.py
+++ pydici/settings.py
@@ -99,13 +99,8 @@ ROOT_URLCONF = 'pydici.urls'
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [os.path.join(PYDICI_ROOTDIR, 'templates')],
+    'APP_DIRS': True,
     'OPTIONS': {
-        'loaders': [
-            ('django.template.loaders.cached.Loader', [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-                ]),
-        ],
         'context_processors': ("django.contrib.auth.context_processors.auth",
                                "django.template.context_processors.debug",
                                "django.template.context_processors.i18n",
@@ -122,9 +117,6 @@ TEMPLATES = [{
 
 if DEBUG:
     TEMPLATES[0]["OPTIONS"]["debug"] = True
-    TEMPLATES[0]["OPTIONS"]["loaders"] = [ # Remove template cache in debug mode
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',]
 
 
 PYDICI_APPS = [
