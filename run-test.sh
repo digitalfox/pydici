bad37ec9ce5c88a3c1bcf1be8d3dd615326eacfb
Sebastien Renard <sebastien.renard@digitalfox.org>
2022-08-27 13:37:21 +0200

    wait mariadb start in GH and fix fs permissions in GH

--- run-test.sh
+++ run-test.sh
@@ -2,4 +2,12 @@
 
 cd $(dirname $0)
 
+if [ -z "$RUNNING_IN_GH"  ];
+then
+	echo "running test in local"
+else
+	echo "inside tests in GH"
+	sleep 10
+fi
+
 PATH=node_modules/.bin/:$PATH OPENSSL_CONF=/etc/ssl python manage.py test $*
