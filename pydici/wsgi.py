47c88a049534f2c50e51f6ceb938aaaf548d59dc
Sebastien Renard <sebastien.renard@digitalfox.org>
2022-08-30 22:12:25 +0200

    ensure pydici path is in sys.path (align with production wsgi setup)

--- pydici/wsgi.py
+++ pydici/wsgi.py
@@ -7,6 +7,13 @@ WSGI Wrapper for production deployment
 
 import os
 
+import sys
+
+pydici_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.pardir)
+if pydici_path not in sys.path:
+    sys.path.append(pydici_path)
+    sys.path.append(os.path.join(pydici_path,"pydici"))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pydici.settings")
 
 # This application object is used by the development server
