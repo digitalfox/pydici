1d2f98ec0e7620d9b74c1ddc267c142bf5d95dcd
Sebastien Renard <sebastien.renard@digitalfox.org>
2022-08-31 19:34:45 +0200

    remove old CRM business broker migration

--- migrate/migrate_new_crm.sql
+++
@@ -1,4 +0,0 @@
-insert into crm_subsidiary select * from core_subsidiary;
-insert into crm_company select * from crm_clientcompany;
-insert into crm_contact select * from crm_clientcontact;
-create table crm_businessbroker_backup as select * from crm_businessbroker;
