80fe41b3612a0c4cc134e57f7c117a4250d05ebf
Sebastien Renard <sebastien.renard@digitalfox.org>
2023-01-02 21:22:20 +0100

    adjust priority to avoid to have too many tasks with highest priority

--- people/tasks.py
+++ people/tasks.py
@@ -8,15 +8,13 @@ Module that handle asynchronous tasks
 
 from datetime import datetime
 
-from django.db.models import Min, Sum, Q
+from django.db.models import Min
 from django.urls import reverse
 from django.utils.translation import gettext as _
 from django.core.cache import cache
 from django.apps import apps
 
 from celery import shared_task
-
-from people.models import CONSULTANT_TASKS_CACHE_KEY
 
 
 @shared_task
@@ -28,6 +26,8 @@ def compute_consultant_tasks(consultant_id):
     - missions with missing financial conditions
     - client bills in draft mode
     - supplier bills to be validated
+
+    :return: list of tasks. Task is a tuple like ("label", count, link, priority)
     """
     # Use get_model to avoid circular imports
     Expense = apps.get_model("expense", "Expense")
@@ -36,6 +36,7 @@ def compute_consultant_tasks(consultant_id):
     ClientBill = apps.get_model("billing", "ClientBill")
     Consultant = apps.get_model("people", "Consultant")
     Lead = apps.get_model("leads", "Lead")
+    from people.models import CONSULTANT_TASKS_CACHE_KEY
 
     consultant = Consultant.objects.get(id=consultant_id)
 
@@ -51,7 +52,7 @@ def compute_consultant_tasks(consultant_id):
     expenses_count = expenses.count()
     if expenses_count > 0:
         expenses_age = (now - expenses.aggregate(Min("update_date"))["update_date__min"]).days
-        expenses_priority = get_task_priority(expenses_age, (5, 10))
+        expenses_priority = get_task_priority(expenses_age, (7, 14))
         tasks.append((_("Expenses to review"), expenses_count, reverse("expense:expenses")+"#managed_expense_workflow_table", expenses_priority))
 
     # Missions without billing mode
@@ -88,7 +89,7 @@ def compute_consultant_tasks(consultant_id):
     bills_count = bills.count()
     if bills_count > 0:
         bills_age = (now.date() - bills.aggregate(Min("creation_date"))["creation_date__min"]).days
-        bills_priority = get_task_priority(bills_age, (2, 5))
+        bills_priority = get_task_priority(bills_age, (5, 10))
         tasks.append((_("Bills to review"), bills_count, reverse("billing:client_bills_in_creation"), bills_priority ))
 
     # Supplier bills to reviews
@@ -96,7 +97,7 @@ def compute_consultant_tasks(consultant_id):
     supplier_bills_count = supplier_bills.count()
     if supplier_bills_count > 0:
         supplier_bills_age = (now.date() - supplier_bills.aggregate(Min("creation_date"))["creation_date__min"]).days
-        supplier_bills_priority = get_task_priority(supplier_bills_age, (2, 5))
+        supplier_bills_priority = get_task_priority(supplier_bills_age, (4, 7))
         tasks.append((_("Supplier bills to review"), supplier_bills_count, reverse("billing:bill_review")+"#supplier_soondue_bills", supplier_bills_priority))
 
     # update cache with computed tasks
