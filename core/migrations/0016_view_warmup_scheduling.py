8c8c275552855be991ae045dedfcd935a988b2d7
Sebastien Renard <sebastien.renard@digitalfox.org>
2023-05-18 17:50:30 +0200

    schedule view warmup task

---
+++ core/migrations/0016_view_warmup_scheduling.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+from django.db import migrations, models
+from django.conf import settings
+
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
+
+
+def add_warmup_task(apps, schema_editor):
+    c = CrontabSchedule.objects.create(minute=0, hour=6, day_of_week="*",
+                                       day_of_month="*", month_of_year="*",
+                                       timezone=settings.TIME_ZONE)
+    t = PeriodicTask.objects.create(name="view warmup", task="core.tasks.view_warmup", crontab=c)
+
+
+
+def remove_warmup_task(apps, schema_editor):
+    t = PeriodicTask.objects.get(name="view warmup", task="core.tasks.view_warmup")
+    t.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0015_drop_actionset"),
+        ("django_celery_beat", "0016_alter_crontabschedule_timezone")
+    ]
+
+    operations = [
+        migrations.RunPython(add_warmup_task, remove_warmup_task),
+    ]
