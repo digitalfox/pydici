9e5744d7644412e515d60fdc453caf3406bd6d43
Sebastien Renard <sebastien.renard@digitalfox.org>
2023-12-02 18:07:22 +0100

    add demo data for contact, client and organisation, linked to client company

--- crm/factories.py
+++ crm/factories.py
@@ -9,9 +9,10 @@ Test data factories for crm module
 from factory.django import DjangoModelFactory
 import factory.django
 import factory.fuzzy
+import random
 
 from people.models import Consultant
-from crm.models import BusinessSector
+from crm.models import BusinessSector, Client
 
 class AbstractAddressFactory(DjangoModelFactory):
     street = factory.Faker("street_address")
@@ -41,9 +42,40 @@ class SubsidiaryFactory(AbstractCompanyFactory):
     class Meta:
         model = "crm.Subsidiary"
 
+class ContactFactory(DjangoModelFactory):
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone = factory.Faker("phone_number")
+    mobile_phone = factory.Faker("phone_number")
+    fax = factory.Faker("phone_number")
+    function = factory.fuzzy.FuzzyChoice(["director", "manager", "head of department", "project director"])
+
+    @factory.post_generation
+    def contact_points(obj, create, extracted, **kwargs):
+        contacts = random.choices(Consultant.objects.all(), k=random.choice([1, 2, 3]))
+        obj.contact_points.set(contacts)
+
+    class Meta:
+        model = "crm.Contact"
+
+class ClientFactory(DjangoModelFactory):
+    expectations = factory.fuzzy.FuzzyChoice(Client.EXPECTATIONS, getter=lambda c: c[0])
+    alignment = factory.fuzzy.FuzzyChoice(Client.ALIGNMENT, getter=lambda c: c[0])
+    contact = factory.SubFactory(ContactFactory)
+    class Meta:
+        model = "crm.Client"
+
+class ClientOrganisationFactory(DjangoModelFactory):
+    name = factory.Iterator(["IT", "digital", "marketing", "supply chain", "board", "finance", "HR"])
+    client = factory.RelatedFactory(ClientFactory, factory_related_name="organisation")
+    class Meta:
+        model = "crm.ClientOrganisation"
+
 class CompanyFactory(AbstractCompanyFactory):
     businessOwner = factory.fuzzy.FuzzyChoice(Consultant.objects.filter(profil__level__gt=0))
     business_sector = factory.fuzzy.FuzzyChoice(BusinessSector.objects.all())
+    clientorganisation = factory.RelatedFactoryList(ClientOrganisationFactory,  factory_related_name="company", size=2)
 
     class Meta:
         model = "crm.Company"
+
