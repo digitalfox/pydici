832959da2dc8cd0a195ac2ca45818f98218e03f6
Sebastien Renard <sebastien.renard@digitalfox.org>
2023-12-03 21:48:21 +0100

    remove force_insert kwargs to avoid duplicate clash when creating bill with .create()

--- billing/models.py
+++ billing/models.py
@@ -141,6 +141,7 @@ class AbstractBill(models.Model):
 
     def save(self, *args, **kwargs):
         super(AbstractBill, self).save(*args, **kwargs)  # Save it
+        if "force_insert" in kwargs: kwargs.pop("force_insert")
         if self.lead.responsible:
             compute_consultant_tasks.delay(self.lead.responsible.id)
 
@@ -228,6 +229,7 @@ class ClientBill(AbstractBill):
 
     def save(self, *args, **kwargs):
         super(ClientBill, self).save(*args, **kwargs)  # Save it to create pk
+        if "force_insert" in kwargs: kwargs.pop("force_insert")
         if not self.lang:
             self.lang = self.lead.client.organisation.billing_lang
         if self.client_deal_id == "":  # First, try with mission client_deal_id if defined and if only one mission
