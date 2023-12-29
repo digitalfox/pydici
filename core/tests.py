41a46c9af649cb92d3c49a1372bbf511b0e10192
Sebastien Renard <sebastien.renard@digitalfox.org>
2023-12-29 22:56:34 +0100

    upgrade selenium and webdriver. Add more quirks args to avoid chromium crash during test suite

--- core/tests.py
+++ core/tests.py
@@ -15,7 +15,7 @@ from django.conf import settings
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import ChromiumOptions
 
 
@@ -285,6 +285,8 @@ class SeleniumTestCase(StaticLiveServerTestCase):
         options = ChromiumOptions()
         options.headless = True
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         self.driver = webdriver.Chrome(
             service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
 
