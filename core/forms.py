ce6e5d3aea2c3e718cd6ff906753ec40b4813226
Sebastien Renard <sebastien.renard@digitalfox.org>
2026-03-02 18:54:54 +0100

    fix token separator to allow tags with space and ' in name

--- core/forms.py
+++ core/forms.py
@@ -26,7 +26,7 @@ class PydiciSelect2WidgetMixin(object):
 
     def build_attrs(self, base_attrs, extra_attrs=None):
         """Set select2's attributes."""
-        default_attrs = {"data-minimum-input-length": 0, "data-theme": "bootstrap-5", "data-width": "auto"}
+        default_attrs = {"data-minimum-input-length": 0, "data-theme": "bootstrap-5", "data-width": "auto", "data-token-separators": '[","]'}
         default_attrs.update(base_attrs)
         attrs = super().build_attrs(default_attrs, extra_attrs=extra_attrs)
         return attrs
