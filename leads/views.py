3af25735f094df4f7fc0470b2dd0eb32efad2bc4
Sebastien Renard <sebastien.renard@digitalfox.org>
2026-03-23 13:56:40 +0100

    order activities by creation date by default

--- leads/views.py
+++ leads/views.py
@@ -348,6 +348,7 @@ def activities(request):
     filter = ActivityFilter(request.GET, queryset=Activity.objects.all(), request=request)
     return render(request, "leads/activities.html",
                   {"data_url" : reverse('leads:activity_table_DT'),
+                   "datatable_options": ''' "order": [[7, "desc"]]  ''',
                    "filter": filter,
                    "filter_form_helper": ActivityFilterFormHelper(),
                    "request_params": request.GET,
