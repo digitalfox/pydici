7b7890bf61d2d0f6844eb6e01231f18a0aa0afca
Sebastien Renard <sebastien.renard@digitalfox.org>
2026-07-03 20:41:48 +0200

    add simple migration script to ensure old pydici database are properly migrated to utf8mb4

---
+++ scripts/utf8-mb4.sql
@@ -0,0 +1,26 @@
+/* produce SQL migration code to ensure utf8mb4 character set and collation for tables and columns */
+/* kindly inspired by zabbix migration scripts */
+SELECT   CONCAT('ALTER TABLE ', table_schema, '.`', table_name, '`',
+		                   ' MODIFY COLUMN `', column_name, '` ', column_type,
+		                   ' CHARACTER SET utf8mb4 COLLATE utf8mb4_bin',
+		                case
+		                    when column_default is null then ''
+		                    else concat(' default ', column_default, ' ')
+		                end,
+		                case
+		                    when is_nullable = 'no' then ' not null '
+		                    else ''
+		                end,
+		            ';') AS command
+FROM information_schema.columns
+WHERE table_schema = 'pydici'
+     AND (column_type like 'varchar%'
+            OR column_type like 'char%'
+            OR column_type = 'longtext')
+
+union
+
+SELECT CONCAT('ALTER TABLE ', table_schema, '.`', table_name, '`',
+		                   ' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;') AS command
+		        FROM information_schema.tables
+		        WHERE table_schema = 'pydici'
