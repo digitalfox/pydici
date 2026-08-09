0aea64dc595da9f2e147e201e078e7129cf7ede2
Sebastien Renard <sebastien.renard@digitalfox.org>
2026-08-09 21:22:03 +0200

    really local other local config files

--- pydici/settings/__init__.py
+++ pydici/settings/__init__.py
@@ -5,6 +5,7 @@
 """
 
 import os
+import sys
 
 from split_settings.tools import optional, include
 
@@ -12,13 +13,13 @@ env = os.environ.get("PYDICI_ENV", "prod")
 
 if env not in ("dev", "prod"):
     print("You need to set PYDICI_ENV environment variable to 'dev' or 'prod'")
-    exit()
+    sys.exit()
 
-include("pydici.py", "django.py", f"{env}.py", optional("local.py"))
+include("pydici.py", "django.py", f"{env}.py", optional("local.py"), optional("local-*.py"))
 
 
 try:
     SECRET_KEY
 except NameError as e:
     print(f"{e}. You need to declare it in settings in your settings/local.py file")
-    exit()
+    sys.exit()
