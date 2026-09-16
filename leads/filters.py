edf3ee58a193a3c954f94aa4cb0bb473174c68e9
Sebastien Renard <sebastien.renard@digitalfox.org>
2026-09-16 17:37:05 +0200

    add marketing product filter on activity page

--- leads/filters.py
+++ leads/filters.py
@@ -4,23 +4,24 @@ Leads filters
 @author: Sébastien Renard <Sebastien.Renard@digitalfox.org>
 @license: AGPL v3 or newer (http://www.gnu.org/licenses/agpl-3.0.html)
 """
-from datetime import date, timedelta
-
 from django.utils.translation import gettext as _
-from django.db.models import Q
 
-from django_filters import FilterSet, ChoiceFilter
+from django_filters import FilterSet, ChoiceFilter, ModelMultipleChoiceFilter
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Row, Column, Field
+from django_select2.forms import Select2MultipleWidget
+
 
 from leads.models import Activity
 from people.models import Consultant
+from staffing.models import MarketingProduct
 
 
 class ActivityFilter(FilterSet):
     responsible = ChoiceFilter(method="responsible_filter")
     state = ChoiceFilter(method="state_filter",
         choices=(Activity.STATES + (("LATE", _("Late")), ("SOON", _("Soon")))))
+    marketing_products = ModelMultipleChoiceFilter(widget=Select2MultipleWidget, queryset=MarketingProduct.objects.all())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -53,16 +54,17 @@ class ActivityFilter(FilterSet):
 
     class Meta:
         model = Activity
-        fields = ["state", "objective", "nature", "responsible"]
+        fields = ["state", "objective", "nature", "responsible", "marketing_products"]
 
 
 class ActivityFilterFormHelper(FormHelper):
     form_method = 'GET'
     layout = Layout(Row(
-        Column(Field("state"), css_class="col-md-2"),
-        Column(Field("objective"), css_class="col-md-2"),
-        Column(Field("nature"), css_class="col-md-2"),
-        Column(Field("responsible"), css_class="col-md-2"),
+        Column(Field("state"), css_class="col-md-3 col-lg-1"),
+        Column(Field("objective"), css_class="col-md-3 col-lg-2"),
+        Column(Field("nature"), css_class="col-md-3 col-lg-2"),
+        Column(Field("responsible"), css_class="col-md-3 col-lg-2"),
+        Column(Field("marketing_products"), css_class="col-md-3 col-lg-3"),
         Column(Submit('submit', _('Apply Filter'), css_class="filter-submit-btn"), css_class="col-md-2"),
         css_class="my-3"
     ))
